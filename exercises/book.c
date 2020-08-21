e116e2d8957f264b4610e8a9431de0178cc32c57
Valery <56233114+mishanyaxd@users.noreply.github.com>
2020-08-21 14:39:43 +0300

    practice

---
+++ exercises/book.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define BOOK "War and Peace"
+
+int main(void)
+{
+    float cost = 12.99;
+    float percent = 80.0;
+
+    printf("This copy of \"%s\" sells for $%2.2f.\n"
+           "That is %.f%% of list.\n",
+            BOOK, cost, percent);
+
+    return 0;
+}
